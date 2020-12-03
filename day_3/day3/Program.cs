21ee6d1317175bd0868b3fa250704f2b33db7248
frogfather <j.a.campbell@icloud.com>
2020-12-03 22:05:44 +0000

    part two

--- day_3/day3/Program.cs
+++ day_3/day3/Program.cs
@@ -8,36 +8,23 @@ namespace day3
     {
         static void Main(string[] args)
         {
-            int xpos = 0;
-            int ypos = 0;
-            int mapWidth = 0;
-            int mapHeight = 0;
-            int blockNumber = 0;
-            int trees = 0;
             ArrayList map = readFile("/Users/johnPersonal/Documents/code/advent/day_3/day3/data.txt");
-            if (map.Count > 0)
+            int[] xOffsets = { 1, 3, 5, 7, 1 };
+            int[] yOffsets = { 1, 1, 1, 1, 2 };
+            int[] results = new int[5];
+            for (int run = 0; run < 5; run ++)
             {
-                mapWidth = (map[0].ToString()).Length;
-                mapHeight = map.Count;
+                results.SetValue(goTobogganing(map, xOffsets[run], yOffsets[run]), run);
             }
-            while (ypos < mapHeight - 1)
-            {
-                ypos += 1;
-                xpos += 3;
-                if (xpos >= mapWidth)
-                {
-                    xpos = xpos - mapWidth;
-                    blockNumber += 1;
-                }
-                //have we hit a tree?
-                if ((map[ypos].ToString())[xpos] == '#')
-                {
-                    trees += 1;
-                }
-            }
-            Console.WriteLine("We have hit " + trees + " trees");
-        }
 
+            Console.WriteLine("Results {0}, {1}, {2}, {3}, {4}", results.GetValue(0), results.GetValue(1), results.GetValue(2), results.GetValue(3), results.GetValue(4));
+            long product = results[0];
+            for (int i = 1; i < 5; i++)
+            {
+                product = product * results[i];
+            }
+            Console.WriteLine("Product of all 5 answers is {0}", product);
+        }
 
         static ArrayList readFile(string filename)
         {
@@ -53,6 +40,40 @@ namespace day3
                 return data;
             }
 
+        }
+
+        static int goTobogganing(ArrayList map, int xOffset, int yOffset)
+        {
+            int xpos = 0;
+            int ypos = 0;
+            int mapWidth = 0;
+            int mapHeight = 0;
+            int blockNumber = 0;
+            int trees = 0;
+
+            if (map.Count > 0)
+            {
+                mapWidth = (map[0].ToString()).Length;
+                mapHeight = map.Count;
+            }
+            Console.WriteLine("map is {0} by {1}", mapWidth, mapHeight);
+            Console.WriteLine("offsets are {0} and {1}", xOffset, yOffset);
+            while (ypos < mapHeight - 1)
+            {
+                ypos += yOffset;
+                xpos += xOffset;
+                if (xpos >= mapWidth)
+                {
+                    xpos = xpos - mapWidth;
+                    blockNumber += 1;
+                }
+                //have we hit a tree?
+                if ((ypos < mapHeight) && (map[ypos].ToString())[xpos] == '#')
+                {
+                    trees += 1;
+                }
+            }
+            return trees;
         }
 
     }
