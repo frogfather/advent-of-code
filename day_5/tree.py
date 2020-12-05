da6c4fccdb9930bf2f03b01aa0a89537c9fc832d
frogfather <j.a.campbell@icloud.com>
2020-12-05 18:20:30 +0000

    much simpler

--- day_5/tree.py
+++ day_5/tree.py
@@ -1,70 +1,60 @@
-class Node:
 
-    def __init__(self, name, low, high):
-    	self.left = None
-	self.right = None
-	self.name = name
-        self.low = low
-	self.high = high
+def FindTheRow(input):
+    rowData = input[:7]
+    sum = 0
+    for i in range( len(rowData) ):
+        divisor = 2 ** i
+        bitValue = 64 // divisor
+        if rowData[i] == "B":
+            sum += bitValue
+    print('Sum for '+rowData+' is '+str(sum))        
+    return sum  
 
-    def insertLeft(self, name, low, high):
-        print('insert left node called '+name+' into '+self.name+' with range '+str(low)+':'+str(high))
-        self.left = Node(name, low, high) 
-        return self.left        
+def FindTheSeat(input):
+    seatData = input[-4:]
+    print('seat data from '+input+' is '+seatData);
+    sum = 0
+    for i in range( len(seatData) ):
+        divisor = 2 ** i
+        bitValue = 4 // divisor
+        if seatData[i] == "R":
+            sum += bitValue
+    print('Sum for '+seatData+' is '+str(sum))
+    return sum
 
-    def insertRight(self, name, low, high):
-        print('insert right node called '+name+' into '+self.name+' with range '+str(low)+':'+str(high))
-        self.right = Node(name, low, high)
-        return self.right
+def ReadTheFile(filename):
+    file = open(filename)
+    fileList = file.readlines()
+    return fileList
 
-    def PrintTree(self):
-        print('Node: '+self.name+' has range: '+str(self.low)+ ':' + str(self.high))
-        if self.left:
-            self.left.PrintTree()
-        # print('Node: '+self.name+' has range: '+str(self.low)+ ':' + str(self.high)),
-        if self.right:
-            self.right.PrintTree()
+def FindMaxSeatId(fileList, max):
+    maxSeatId = 0
+    occupiedSeats = []
+    for entry in fileList:
+       calculatedRow = FindTheRow(entry)
+       calculatedSeat = FindTheSeat(entry)
+       seatId = calculatedRow * 8 + calculatedSeat
+       occupiedSeats.append(seatId)
+       if seatId > maxSeatId:
+           maxSeatId = seatId
+    if max == True:
+        return maxSeatId
+    else:
+        print('returning list');
+        return occupiedSeats
 
-    def AddNodes(self, node):
-        if  node.high != node.low:
-            leftLow = node.low
-            rightLow = node.low + (node.high + 1 - node.low)//2 
-            rightHigh = node.high
-            leftHigh = rightLow - 1
-            leftName = str(leftLow) + '-' + str(leftHigh)
-            rightName = str(rightLow) + '-' + str(rightHigh)
-            newLeft = node.insertLeft(leftName, leftLow, leftHigh)
-            node.AddNodes(newLeft)
-            newRight = node.insertRight(rightName, rightLow, rightHigh)
-            node.AddNodes(newRight)
+def FindUnoccupiedSeats(occupiedList):
+    unoccupiedSeats = []
+    for x in range(1024):
+        if x not in occupiedList:
+            print(str(x)+' not in occupiedList')
+            unoccupiedSeats.append(x)
+    return unoccupiedSeats        
 
-    def FindTheRow(self, input):
-        rowData = input[:7]
-        print('Input is '+rowData)
-        currentNode = self
-        for letter in rowData:
-            if letter == "F":
-                currentNode = currentNode.left
-            else:
-                currentNode = currentNode.right            
-        print('Input corresponds to row '+str(currentNode.low))        
 
-    def ReadTheFile(self, filename):
-        file = open(filename)
-        fileList = file.readlines()
-        print('List contains '+str(len(fileList))+' items')
-        return fileList
-
-    def FindTheRows(self, fileList):
-       results = []
-       for row in fileList:
-           results.append(root.FindTheRow(row))
-       print('there are '+str(len(results))+' results')
-       return results
-
-root = Node('root', 0, 127)
-root.AddNodes(root)
-# root.PrintTree()
-fileList = root.ReadTheFile("data.txt")
-rowsFound = root.FindTheRows(fileList)
-
+fileList = ReadTheFile("data.txt")
+maxSeatId = FindMaxSeatId(fileList, True)
+occupiedList = FindMaxSeatId(fileList, False)
+print('The max seat id is '+str(maxSeatId))
+unoccupiedList = FindUnoccupiedSeats(occupiedList)
+print('The unoccupied list contains '+str(len(unoccupiedList))+' items')
