67de98fe4e2294e0b2525265ef28545daf8c04b2
frogfather <j.a.campbell@icloud.com>
2021-12-28 00:14:50 +0000

    change array to integer

--- 2021/backup/bingo.pas
+++ 2021/backup/bingo.pas
@@ -7,13 +7,13 @@ interface
 uses
   Classes, SysUtils,aocUtils,arrayUtils;
 type
-  ABingoCard = array of array of array of string;
+  BingoGrid = array of array of array of integer;
 
   { TBingoCard }
   TBingoCard = class(TInterfacedObject)
     private
     fId: Integer;
-    fCard: ABingoCard;
+    fCard: BingoGrid;
     fLastCalled: integer;
     fUncalledSum:integer;
     fColCount:integer;
@@ -104,19 +104,6 @@ begin
    end;
 end;
 
-function TBingoGame.getDimensionsOfPuzzleInput(input: TStringArray): TPoint;
-begin
-  result.X:=0;
-  result.Y:=0;
-  if length(input) = 0 then exit;
-  with result do
-    begin
-    Y:= length(input);
-    X:= length(input[0]);
-    end;
-end;
-
-
 constructor TBingoGame.create(puzzleInput: TStringArray);
 begin
   fNumbersToCall:=puzzleInput[0].Split(',');
@@ -188,7 +175,7 @@ var
 begin
   bits:=TBits.Create(fColCount);
   for col := 0 to pred(fColCount) do
-  bits[col]:= fCard[row][col][1] = '1';
+  bits[col]:= fCard[row][col][1] = 1;
   result:=bits;
 end;
 
@@ -199,14 +186,14 @@ var
 begin
   bits:=TBits.Create(fRowCount);
   for row := 0 to pred(fRowCount) do
-  bits[row]:= fCard[row][col][1] = '1';
+  bits[row]:= fCard[row][col][1] = 1;
   result:=bits;
 end;
 
 procedure TBingoCard.calculateUncalled;
 var
   row,col,uncalledCount:integer;
-  uncalledValue:string;
+  uncalledValue:integer;
 begin
   //In theory we could set fUncalledSum to the number of elements at the start
   //and decrement with each call, but that doesn't handle the same number being
@@ -216,11 +203,11 @@ begin
     begin
     for col:=0 to pred(fColCount) do
       begin
-      if fCard[row][col][1]='0'
+      if fCard[row][col][1] = 0
         then
           begin
           uncalledValue:=fCard[row][col][0];
-          uncalledCount:=uncalledCount+strToInt(fCard[row][col][0])
+          uncalledCount:=uncalledCount + fCard[row][col][0]
           end;
 
       end;
@@ -230,17 +217,15 @@ end;
 
 procedure TBingoCard.call(number: integer);
 var
-  sNumber:string;
   col,row:integer;
 begin
   //if the supplied number matches one of the numbers on our card
   //set the second element of the third dimension(?) to '1'
   //and call checkWin
-  sNumber:=number.ToString;
   for row:=0 to pred(fRowCount) do
     begin
     for col:=0 to pred(fColCount) do
-      if (fCard[row][col][0] = sNumber) then fCard[row][col][1]:='1';
+      if (fCard[row][col][0] = number) then fCard[row][col][1]:= 1;
     end;
   if not fWinning and checkWin then
     begin
@@ -261,7 +246,7 @@ fRowCount:=length(input);
 if (fRowCount > 0) then currElements:= input[0].Split(' ',TStringSplitOptions.ExcludeEmpty) else currElements:=TStringArray.Create;
 fcolCount:=length(currElements);
 fWinning:=false;
-fCard:=ABingoCard.create;
+fCard:=BingoGrid.create;
 fNotifyCardWin:=eventHandler;
 fId:=cardId;//we need this to identify which card has won
 //set the three dimensions of the array
--- 2021/bingo.pas
+++ 2021/bingo.pas
@@ -7,13 +7,13 @@ interface
 uses
   Classes, SysUtils,aocUtils,arrayUtils;
 type
-  ABingoCard = array of array of array of string;
+  BingoGrid = array of array of array of integer;
 
   { TBingoCard }
   TBingoCard = class(TInterfacedObject)
     private
     fId: Integer;
-    fCard: ABingoCard;
+    fCard: BingoGrid;
     fLastCalled: integer;
     fUncalledSum:integer;
     fColCount:integer;
@@ -175,7 +175,7 @@ var
 begin
   bits:=TBits.Create(fColCount);
   for col := 0 to pred(fColCount) do
-  bits[col]:= fCard[row][col][1] = '1';
+  bits[col]:= fCard[row][col][1] = 1;
   result:=bits;
 end;
 
@@ -186,14 +186,14 @@ var
 begin
   bits:=TBits.Create(fRowCount);
   for row := 0 to pred(fRowCount) do
-  bits[row]:= fCard[row][col][1] = '1';
+  bits[row]:= fCard[row][col][1] = 1;
   result:=bits;
 end;
 
 procedure TBingoCard.calculateUncalled;
 var
   row,col,uncalledCount:integer;
-  uncalledValue:string;
+  uncalledValue:integer;
 begin
   //In theory we could set fUncalledSum to the number of elements at the start
   //and decrement with each call, but that doesn't handle the same number being
@@ -203,11 +203,11 @@ begin
     begin
     for col:=0 to pred(fColCount) do
       begin
-      if fCard[row][col][1]='0'
+      if fCard[row][col][1] = 0
         then
           begin
           uncalledValue:=fCard[row][col][0];
-          uncalledCount:=uncalledCount+strToInt(fCard[row][col][0])
+          uncalledCount:=uncalledCount + fCard[row][col][0]
           end;
 
       end;
@@ -217,17 +217,15 @@ end;
 
 procedure TBingoCard.call(number: integer);
 var
-  sNumber:string;
   col,row:integer;
 begin
   //if the supplied number matches one of the numbers on our card
   //set the second element of the third dimension(?) to '1'
   //and call checkWin
-  sNumber:=number.ToString;
   for row:=0 to pred(fRowCount) do
     begin
     for col:=0 to pred(fColCount) do
-      if (fCard[row][col][0] = sNumber) then fCard[row][col][1]:='1';
+      if (fCard[row][col][0] = number) then fCard[row][col][1]:= 1;
     end;
   if not fWinning and checkWin then
     begin
@@ -248,7 +246,7 @@ fRowCount:=length(input);
 if (fRowCount > 0) then currElements:= input[0].Split(' ',TStringSplitOptions.ExcludeEmpty) else currElements:=TStringArray.Create;
 fcolCount:=length(currElements);
 fWinning:=false;
-fCard:=ABingoCard.create;
+fCard:=BingoGrid.create;
 fNotifyCardWin:=eventHandler;
 fId:=cardId;//we need this to identify which card has won
 //set the three dimensions of the array
@@ -258,8 +256,8 @@ for currLine:=0 to pred(fRowCount) do
   currElements:=input[currLine].Split(' ',TStringSplitOptions.ExcludeEmpty);
   for currCol:=0 to pred(fColCount) do
     begin
-    fCard[currLine][currCol][0]:=currElements[currCol];
-    fCard[currLine][currCol][1]:='0';
+    fCard[currLine][currCol][0]:= currElements[currCol].ToInteger;
+    fCard[currLine][currCol][1]:= 0;
     end;
   end;
 end;
