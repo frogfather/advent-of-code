3add658733789171992b354857371c5f23ec5450
frogfather <j.a.campbell@icloud.com>
2022-12-08 21:09:53 +0000

    part two done - ugly but working

--- 2022/day8.pas
+++ 2022/day8.pas
@@ -14,9 +14,11 @@ TDayEight = class(TAocPuzzle)
   private
     fTreeMap:T3DIntMap;
     procedure initializeMap;
-    procedure checkSingleTreeView(x,y:integer);
-    procedure checkTreeViews;
+    procedure checkIfThisTreeHasView(x,y:integer);
+    procedure checkTrees(scores:boolean=false);
     function getAllTreesWithView:integer;
+    procedure findThisTreeScore(x,y:integer);
+    function getTreeWithHighestScore:integer;
   public
     constructor Create(filename: string; paintbox_: TPaintbox = nil);
     procedure runPartOne; override;
@@ -40,26 +42,76 @@ begin
     end;
 end;
 
-procedure TDayEight.checkSingleTreeView(x,y:integer);
+procedure TDayEight.findThisTreeScore(x, y: integer);
+var
+  thisTreeHeight,thisTreeScore,xIndex,yIndex,dirScore:integer;
+begin
+ //we find how many trees in each direction are smaller than this one
+ //if a tree is bigger or the same size we stop (and include that one)
+ thisTreeHeight:=map[x][y][0];
+
+ thisTreeScore:=1;
+ //North
+ dirScore:=0;
+ if (y > 0) then
+   for yIndex:=(y-1) downto 0 do
+   begin
+    dirScore:=dirScore + 1;
+    if (map[x][yIndex][0] >= thisTreeHeight) then break;
+    end;
+ thisTreeScore:=thisTreeScore * dirScore;
+ dirScore:=0;
+ //South
+ if (y < pred(length(map[x]))) then
+   for yIndex:= (y+1) to pred(length(map[x])) do
+   begin
+    dirScore:=dirScore + 1;
+    if (map[x][yIndex][0] >= thisTreeHeight) then break;
+    end;
+ thisTreeScore:=thisTreeScore * dirScore;
+ dirScore:=0;
+ //West
+ if (x > 0) then
+   for xIndex:=(x-1) downto 0 do
+   begin
+    dirScore:=dirScore + 1;
+    if (map[xIndex][y][0] >= thisTreeHeight) then break;
+    end;
+ thisTreeScore:=thisTreeScore * dirScore;
+ dirScore:=0;
+ //East
+ if (x < pred(length(map))) then
+   for xIndex:= (x+1) to pred(length(map)) do
+   begin
+    dirScore:=dirScore + 1;
+    if (map[xIndex][y][0] >= thisTreeHeight) then break;
+    end;
+ thisTreeScore:=thisTreeScore * dirScore;
+
+ results.add('tree at '+x.toString+' '+y.ToString+' has score '+thisTreeScore.toString);
+ map[x][y][1]:=thisTreeScore;
+end;
+
+procedure TDayEight.checkIfThisTreeHasView(x,y:integer);
 var
   thisTreeHeight,checkTreeHeight,xIndex,yIndex:integer;
   viewInThisDirection:boolean;
 begin
   thisTreeHeight:=map[x][y][0];
-  //from this point is there any path (nsew) where all trees ahead are smaller
-  if (x=0) or (y=0) then //if we're on an edge there's always a view
+  if (x=0) or (y=0) then
     begin
       map[x][y][1]:=1;
       exit;
     end;
-  //get the distances from each edge
-  //TODO arrange with shortest first
-  //For the moment just do them in order
   viewInThisDirection:=true;
   for xIndex:=(x-1) downTo 0 do
     begin
       checkTreeHeight:= map[xIndex][y][0];
-    if (checkTreeHeight >= thisTreeHeight) then viewInThisDirection:=false;
+    if (checkTreeHeight >= thisTreeHeight) then
+      begin
+      viewInThisDirection:=false;
+      break;
+      end;
     end;
   if viewInThisDirection then
     begin
@@ -70,7 +122,11 @@ begin
   for xIndex:=(x+1) to pred(length(map)) do
     begin
     checkTreeHeight:= map[xIndex][y][0];
-    if (checkTreeHeight >= thisTreeHeight) then viewInThisDirection:=false;
+    if (checkTreeHeight >= thisTreeHeight) then
+      begin
+      viewInThisDirection:=false;
+      break;
+      end;
     end;
   if viewInThisDirection then
     begin
@@ -81,7 +137,11 @@ begin
   for yIndex:=(y-1) downto 0 do
     begin
     checkTreeHeight:= map[x][yIndex][0];
-    if (checkTreeHeight >= thisTreeHeight) then viewInThisDirection:=false;
+    if (checkTreeHeight >= thisTreeHeight) then
+      begin
+      viewInThisDirection:=false;
+      break;
+      end;
     end;
   if viewInThisDirection then
     begin
@@ -92,7 +152,11 @@ begin
   for yIndex:= (y+1) to pred(length(map[x])) do
     begin
     checkTreeHeight:= map[x][yIndex][0];
-    if (checkTreeHeight >= thisTreeHeight) then viewInThisDirection:=false;
+    if (checkTreeHeight >= thisTreeHeight) then
+      begin
+      viewInThisDirection:=false;
+      break;
+      end;
     end;
   if viewInThisDirection then
     begin
@@ -101,14 +165,17 @@ begin
     end;
 end;
 
-procedure TDayEight.checkTreeViews;
+procedure TDayEight.checkTrees(scores:boolean);
 var
   x,y:integer;
 begin
   //go through all trees and check if there is a view
   for x:= 0 to pred(length(map)) do
     for y:= 0 to pred(length(map[x])) do
-      checkSingleTreeView(x,y);
+      begin
+      if scores then findThisTreeScore(x,y)
+        else checkIfThisTreeHasView(x,y);
+      end;
 end;
 
 function TDayEight.getAllTreesWithView: integer;
@@ -123,6 +190,16 @@ begin
   result:=visibleTrees;
 end;
 
+function TDayEight.getTreeWithHighestScore: integer;
+var
+  x,y:integer;
+begin
+  result:=0;
+  for x:=0 to pred(length(map)) do
+    for y:=0 to pred(length(map[x])) do
+      if (map[x][y][1] > result) then result:= map[x][y][1];
+end;
+
 constructor TDayEight.Create(filename: string; paintbox_: TPaintbox);
 begin
   inherited Create(filename,'Day 8',paintbox_);
@@ -134,13 +211,15 @@ begin
   //for each tree in the map we want to see if there is any path left/right/up/down
   //where all the trees in front of it are smaller
   initializeMap;
-  checkTreeViews;
+  checkTrees;
   results.add('visible trees '+getAllTreesWithView.toString);
 end;
 
 procedure TDayEight.runPartTwo;
 begin
-
+  initializeMap;
+  checkTrees(true);
+  results.Add('Tree with highest score '+getTreeWithHighestScore.toString);
 end;
 
 end.
