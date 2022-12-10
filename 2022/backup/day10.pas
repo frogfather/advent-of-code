e511dc60cf32628c07e7d563648faad41a2b5e3d
frogfather <j.a.campbell@icloud.com>
2022-12-10 08:09:13 +0000

    combine two puzzle parts since they are largely the same

--- 2022/backup/day10.pas
+++ 2022/backup/day10.pas
@@ -13,7 +13,9 @@ type
 TDayTen = class(TAocPuzzle)
   private
      fScreen:T2DStringArray;
+     fTotalValue:integer;
      procedure drawScreen(sender:TObject);
+     procedure runPuzzle;
   public
     constructor Create(filename: string; paintbox_: TPaintbox = nil);
     procedure runPartOne; override;
@@ -31,6 +33,7 @@ begin
   begin
   canvas.font.Size:=24;
   canvas.Brush.color:=clblack;
+  canvas.Rectangle(0,0,canvas.Width,canvas.Height);
   canvas.Font.Color:=clLime;
   for screenY:=0 to pred(Length(fScreen[0])) do
     for screenX:=0 to pred(length(fScreen)) do
@@ -38,62 +41,15 @@ begin
   end;
 end;
 
-constructor TDayTen.Create(filename: string; paintbox_: TPaintbox);
-begin
-  inherited Create(filename,'Day 10',paintbox_);
-  if (paintbox <> nil) then Paintbox.OnPaint:=@drawScreen;
-  fScreen:=T2DStringArray.create;
-  setLength(fScreen,40,6);
-end;
-
-procedure TDayTen.runPartOne;
-const cyclesToAdd:TIntArray = (20,60,100,140,180,220);
-var
+procedure TDayTen.runPuzzle;
+  const cyclesToAdd:TIntArray = (20,60,100,140,180,220);
+  var
   index:integer;
   instruction:TStringArray;
   X,signalStrength,cycle,addValue:integer;
   done,getNext:boolean;
-  totalValue:integer;
-begin
-  X:=1;
-  cycle:=0;
-  index:=0;
-  addValue:=0;
-  done:=false;
-  getNext:=true;
-  totalValue:=0;
-  while not done do
-    begin
-    cycle:=cycle + 1;
-    if (getNext = true) then
-      begin
-      X:=X+addValue;
-      instruction:=puzzleInputLines[index].Split(' ');
-      if (instruction[0] <> 'noop') then
-        begin
-        getNext:=false;
-        addValue:=instruction[1].toInteger;
-        end else addValue:=0;
-      index:=index+1;
-      done:=(index = puzzleInputLines.size);
-      end else getNext:= true;
-    signalStrength:=X * cycle; //X doesn't change until the end of the cycle
-    results.add('x register at cycle '+cycle.toString+': '+X.ToString);
-    if (cyclesToAdd.indexOf(cycle) > -1) then
-      begin
-      totalValue:=totalValue + signalStrength;
-      results.add('totalValue at cycle '+cycle.ToString+': '+totalValue.toString);
-      end;
-    end;
-end;
-
-procedure TDayTen.runPartTwo;
-var
-  index:integer;
-  instruction:TStringArray;
-  X,cycle,addValue:integer;
-  done,getNext:boolean;
   pixel,displayX,displayY:integer;
+
 begin
   X:=1;
   cycle:=0;
@@ -101,8 +57,7 @@ begin
   addValue:=0;
   done:=false;
   getNext:=true;
-  displayX:=0;
-  displayY:=0;
+  fTotalValue:=0;
   while not done do
     begin
     cycle:=cycle + 1;
@@ -116,9 +71,15 @@ begin
         addValue:=instruction[1].toInteger;
         end else addValue:=0;
       index:=index+1;
-      done:=(index = puzzleInputLines.size);
       end else getNext:= true;
-    //if the current pixel (cycle mod 40) is in range (x-1, x+1) then we draw # otherwise we draw '.'
+    done:=(index = puzzleInputLines.size) and (getNext = true);
+    //Used in part 1
+    signalStrength:=X * cycle;
+    if (cyclesToAdd.indexOf(cycle) > -1) then
+       fTotalValue:=totalValue + signalStrength;
+
+    //Used in part 2
+    //if the current pixel ((cycle-1) mod 40) is in range (x-1, x+1) then we draw # otherwise we draw '.'
     pixel:=(cycle-1) mod 40;
     if (pixel >= x-1)and(pixel <= x+1) then
        fScreen[displayX][displayY]:='#'
@@ -130,6 +91,27 @@ begin
       displayX:=0;
       end;
     end;
+end;
+
+constructor TDayTen.Create(filename: string; paintbox_: TPaintbox);
+begin
+  inherited Create(filename,'Day 10',paintbox_);
+  if (paintbox <> nil) then Paintbox.OnPaint:=@drawScreen;
+  fScreen:=T2DStringArray.create;
+  setLength(fScreen,40,6);
+end;
+
+procedure TDayTen.runPartOne;
+
+begin
+  runPuzzle;
+  results.add('totalValue '+fTotalValue.toString);
+end;
+
+procedure TDayTen.runPartTwo;
+
+begin
+  runPuzzle;
   results.Add('Click Visualise to see the answer');
 end;
 
--- 2022/day10.pas
+++ 2022/day10.pas
@@ -13,7 +13,9 @@ type
 TDayTen = class(TAocPuzzle)
   private
      fScreen:T2DStringArray;
+     fTotalValue:integer;
      procedure drawScreen(sender:TObject);
+     procedure runPuzzle;
   public
     constructor Create(filename: string; paintbox_: TPaintbox = nil);
     procedure runPartOne; override;
@@ -39,62 +41,15 @@ begin
   end;
 end;
 
-constructor TDayTen.Create(filename: string; paintbox_: TPaintbox);
-begin
-  inherited Create(filename,'Day 10',paintbox_);
-  if (paintbox <> nil) then Paintbox.OnPaint:=@drawScreen;
-  fScreen:=T2DStringArray.create;
-  setLength(fScreen,40,6);
-end;
-
-procedure TDayTen.runPartOne;
-const cyclesToAdd:TIntArray = (20,60,100,140,180,220);
-var
+procedure TDayTen.runPuzzle;
+  const cyclesToAdd:TIntArray = (20,60,100,140,180,220);
+  var
   index:integer;
   instruction:TStringArray;
   X,signalStrength,cycle,addValue:integer;
   done,getNext:boolean;
-  totalValue:integer;
-begin
-  X:=1;
-  cycle:=0;
-  index:=0;
-  addValue:=0;
-  done:=false;
-  getNext:=true;
-  totalValue:=0;
-  while not done do
-    begin
-    cycle:=cycle + 1;
-    if (getNext = true) then
-      begin
-      X:=X+addValue;
-      instruction:=puzzleInputLines[index].Split(' ');
-      if (instruction[0] <> 'noop') then
-        begin
-        getNext:=false;
-        addValue:=instruction[1].toInteger;
-        end else addValue:=0;
-      index:=index+1;
-      done:=(index = puzzleInputLines.size);
-      end else getNext:= true;
-    signalStrength:=X * cycle; //X doesn't change until the end of the cycle
-    results.add('x register at cycle '+cycle.toString+': '+X.ToString);
-    if (cyclesToAdd.indexOf(cycle) > -1) then
-      begin
-      totalValue:=totalValue + signalStrength;
-      results.add('totalValue at cycle '+cycle.ToString+': '+totalValue.toString);
-      end;
-    end;
-end;
-
-procedure TDayTen.runPartTwo;
-var
-  index:integer;
-  instruction:TStringArray;
-  X,cycle,addValue:integer;
-  done,getNext:boolean;
   pixel,displayX,displayY:integer;
+
 begin
   X:=1;
   cycle:=0;
@@ -102,6 +57,7 @@ begin
   addValue:=0;
   done:=false;
   getNext:=true;
+  fTotalValue:=0;
   displayX:=0;
   displayY:=0;
   while not done do
@@ -117,9 +73,15 @@ begin
         addValue:=instruction[1].toInteger;
         end else addValue:=0;
       index:=index+1;
-      done:=(index = puzzleInputLines.size);
       end else getNext:= true;
-    //if the current pixel (cycle mod 40) is in range (x-1, x+1) then we draw # otherwise we draw '.'
+    done:=(index = puzzleInputLines.size) and (getNext = true);
+    //Used in part 1
+    signalStrength:=X * cycle;
+    if (cyclesToAdd.indexOf(cycle) > -1) then
+       fTotalValue:=fTotalValue + signalStrength;
+
+    //Used in part 2
+    //if the current pixel ((cycle-1) mod 40) is in range (x-1, x+1) then we draw # otherwise we draw '.'
     pixel:=(cycle-1) mod 40;
     if (pixel >= x-1)and(pixel <= x+1) then
        fScreen[displayX][displayY]:='#'
@@ -131,6 +93,27 @@ begin
       displayX:=0;
       end;
     end;
+end;
+
+constructor TDayTen.Create(filename: string; paintbox_: TPaintbox);
+begin
+  inherited Create(filename,'Day 10',paintbox_);
+  if (paintbox <> nil) then Paintbox.OnPaint:=@drawScreen;
+  fScreen:=T2DStringArray.create;
+  setLength(fScreen,40,6);
+end;
+
+procedure TDayTen.runPartOne;
+
+begin
+  runPuzzle;
+  results.add('totalValue '+fTotalValue.toString);
+end;
+
+procedure TDayTen.runPartTwo;
+
+begin
+  runPuzzle;
   results.Add('Click Visualise to see the answer');
 end;
 
