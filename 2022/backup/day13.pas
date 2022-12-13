685d3c6c6595e7db87c164ec84ca497c3ca9eccd
frogfather <j.a.campbell@icloud.com>
2022-12-13 23:54:37 +0000

    part 2 almost working

--- 2022/backup/day13.pas
+++ 2022/backup/day13.pas
@@ -7,7 +7,7 @@ unit day13;
 interface
 
 uses
-  Classes, SysUtils, aocPuzzle, LazLogger, ExtCtrls, Graphics, arrayUtils,routeFind;
+  Classes, SysUtils, aocPuzzle, LazLogger, ExtCtrls, Graphics, arrayUtils,routeFind,anysort;
 type
   TPacketData = record
     left: string;
@@ -21,8 +21,11 @@ type
   TDayThirteen = class(TAocPuzzle)
   private
     fPacketPairs: TPacketDataArray;
+    fAllPackets: TStringArray;
     procedure getPairs;
+    procedure getPackets;
     procedure runPuzzle;
+    procedure sortPackets(var arr: array of string; count: Integer);
     function separateNumbers(entry:TPacketData):TPacketDataArray;
     function doSeparateNumber(entry:string):TStringArray;
     function testPacket(packetIndex:integer):integer;
@@ -53,23 +56,33 @@ var
 begin
   currentPair.left:='';
   currentPair.right:='';
-  results.add(puzzleInputLines.size.toString+' lines');
   for index:= 0 to pred(puzzleInputLines.size) do
     begin
     currentLine:=puzzleInputLines[index].Trim;
     pairIndex:= (index + 1) mod 3;
-    results.add('index '+index.tostring);
 
     if (pairIndex = 1) then currentPair.left:= currentLine
     else if (pairIndex = 2) then currentPair.right:= currentLine;
     if (pairIndex = 0)or(index = pred(puzzleInputLines.size)) then
       begin
-      results.add('adding '+currentPair.left+'::'+currentPair.right);
       fPacketPairs.push(currentPair);
       currentPair.left:='';
       currentPair.right:='';
       end;
     end;
+end;
+
+procedure TDayThirteen.getPackets;
+var
+  index:integer;
+begin
+  fAllPackets:=TStringArray.create;
+  for index:= 0 to pred(puzzleInputLines.size) do
+    begin
+    if (puzzleInputLines[index]<>'') then fAllPackets.push(puzzleInputLines[index]);
+    end;
+  fAllPackets.push('[[2]]');
+  fAllPackets.push('[[6]]');
 end;
 
 procedure TDayThirteen.runPuzzle;
@@ -79,15 +92,92 @@ var
 begin
   validIndices:=0;
   for pairIndex:=0 to pred(fPacketPairs.size) do
-    begin
-    if (testPacket(pairIndex) = 1) then
-      begin
-      results.add('index '+(pairIndex+1).ToString+' valid');
-      validIndices:=validIndices + pairIndex + 1;
-      end;
-    end;
+    if (testPacket(pairIndex) = 1)
+      then validIndices:=validIndices + pairIndex + 1;
   results.add('valid indices '+validIndices.toString);
 end;
+
+function CompareStr(const d1,d2): integer;
+var
+  s1 : string absolute d1;
+  s2 : string absolute d2;
+  currentS1,currentS2:char;
+  snumber1,snumber2:string;
+  numbers1,numbers2:TIntArray;
+  index,longestString,mostNumbers:integer;
+  openBrS1,openBrS2:integer;
+begin
+  result:=0;
+  numbers1:=TIntArray.create;
+  numbers2:=TIntArray.create;
+  sNumber1:='';
+  sNumber2:='';
+  openBrS1:=0;
+  openBrS2:=0;
+  if s1.Length > s2.Length
+    then longestString:=s1.Length
+    else longestString:=s2.Length;
+  for index:= 1 to longestString do //strings 1 indexed!!!
+    begin
+    if (index <= s1.Length) then
+      begin
+      currentS1:=s1[index];
+      if ord(currentS1) = 91 then openBrS1:= openBrS1+1 else
+      if (ord(currentS1)>47)and(ord(currentS1)<58) then sNumber1:=sNumber1+s1.Substring(index-1,1) else
+      if ((ord(currentS1)=44)or(ord(currentS1) = 93))then
+        begin
+        if (sNumber1.Length > 0) then
+        numbers1.push(sNumber1.ToInteger) else numbers1.push(-1);
+        sNumber1:='';
+        end;
+      end;
+    if (index <= s2.Length) then
+      begin
+      currentS2:=s2[index];
+      if ord(currentS2) = 91 then openBrS2:= openBrS2+1 else //open square bracket
+      if (ord(currentS2)>47)and(ord(currentS2)<58) then sNumber2:=sNumber2+s2.Substring(index-1,1) else //number
+
+      if ((ord(currentS2)=44)or(ord(currentS2) = 93))and(sNumber2.Length > 0)then
+        begin
+        numbers2.push(sNumber2.ToInteger);
+        sNumber2:='';
+        end;
+      end;
+
+    end;
+  //Now we have info about the two strings
+
+  //if neither has numbers then the one with more brackets is bigger
+  if (numbers1.size = 0) and (numbers2.size = 0) and (openBrS1 <> openBrS2)  then
+    result:= (openBrS1 - openBrS2) div abs(openBrS1 - openBrS2) else
+
+  //if one has numbers and the other has none then the one with numbers is bigger
+  if (numbers1.size = 0)and(numbers2.size > 0) then result:= -1
+  else if (numbers1.size > 0)and(numbers2.size = 0) then result:= 1;
+  //if the result is still 0 (neither bigger) then compare the numbers
+  if (result = 0) then
+    begin
+    if numbers1.size > numbers2.size then mostNumbers:= numbers1.size
+    else mostNumbers:=numbers2.size;
+    for index:=0 to pred(mostNumbers) do
+      begin
+      if (index = numbers1.size)then result:= -1 else //1 has no more numbers
+      if (index = numbers2.size) then result:= 1 else //2 has no more numbers
+      if result = 0 then
+        begin
+        if (numbers1[index] <> numbers2[index]) then
+          result:= (numbers1[index] - numbers2[index]) div abs(numbers1[index] - numbers2[index]);
+        if (result <> 0) then exit;
+
+        end;
+      end;
+    end;
+end;
+
+procedure TDayThirteen.sortPackets(var arr: array of string; count: Integer);
+  begin
+    anysort.AnySort(arr, Count, sizeof(string), @CompareStr)
+  end;
 
 function TDayThirteen.testPacket(packetIndex: integer): integer;
 var
@@ -109,7 +199,6 @@ begin
   subPackets:=separateNumbers(entry);
   for index:=0 to pred(subPackets.size) do
     begin
-    results.add('compare '+subPackets[index].left+' to '+subPackets[index].right);
     leftNoValue:= subPackets[index].left.Length = 0;
     rightNoValue:=subPackets[index].right.Length = 0;
     leftIsNumber:=isNumber(subPackets[index].left);
@@ -238,8 +327,25 @@ begin
 end;
 
 procedure TDayThirteen.runPartTwo;
+var
+  divider1,divider2:integer;
+  index:integer;
 begin
-
+  getPackets;
+  sortPackets(fAllPackets,fAllPackets.size);
+  divider1:=-1;
+  divider2:=-1;
+  for index:=0 to pred(fAllPackets.size) do
+    begin
+    if (fAllPackets[index]='[[2]]')then divider1:=index+1 else
+    if (fAllPackets[index]='[[6]]')then divider2:=index+1;
+    results.add(fAllPackets[index]);
+    end;
+  if (divider1 > 0)and(divider2 > 0) then
+      begin
+      results.add('Decoder key '+(divider1*divider2).ToString);
+      exit;
+      end;
 end;
 
 //Helper class for TPacketData
