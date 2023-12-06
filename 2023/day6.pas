ceb753e28a30249515067c01ef6283872b013614
frogfather <buyjohn@gmail.com>
2023-12-06 06:45:09 +0000

    part one

--- 2023/day6.pas
+++ 2023/day6.pas
@@ -27,9 +27,41 @@ inherited create(filename,'Day 6',paintbox_);
 //parent loads the file as a string and converts to string array;
 end;
 
+//Time     x x x
+//distance y y y
 procedure TDaySix.runPartOne;
+var
+  times,distances:TStringArray;
+  raceCount,raceNo,availableTime, recordDistance,timeHeld,speed,distanceTravelled:integer;
+  beatRecordCount,totalBeatRecordCount:integer;
 begin
   results.Clear;
+  //Total time = t
+  //Time held = x
+  //Time running = t - x
+  //speed = x
+  //get the puzzle input
+  times:=puzzleInputLines[0].Split([' '],(TstringSplitOptions.ExcludeEmpty));
+  distances:=puzzleInputLines[1].Split([' '],(TstringSplitOptions.ExcludeEmpty));
+  raceCount:=times.size - 1;
+  totalBeatRecordCount:=0;
+  for raceNo:=1 to raceCount do
+    begin
+    beatRecordCount:=0;
+    availableTime:=times[raceNo].ToInteger;
+    recordDistance:=distances[raceno].ToInteger;
+    for timeHeld:=1 to availableTime - 1 do
+      begin
+      speed:=timeHeld;
+      distanceTravelled:= (availableTime - timeHeld) * speed;
+      results.add('Race: '+raceNo.ToString+' - Speed: '+speed.ToString+', distance: '+distanceTravelled.ToString);
+      if (distanceTravelled > recordDistance) then beatRecordCount:=beatRecordCount+1;
+      end;
+    if (beatRecordCount = 0) then break; //Don't multiply by zero!
+    if (totalBeatRecordCount = 0) then totalBeatRecordCount:=beatRecordCount
+    else totalBeatRecordCount:=totalBeatRecordCount * beatRecordCount;
+    end;
+  results.add('The answer is '+totalbeatrecordCount.ToString);
 end;
 
 procedure TDaySix.runPartTwo;
