f76739dd2d77fe7cc548d2e3d79282a627a6c781
frogfather <buyjohn@gmail.com>
2023-12-14 21:13:01 +0000

    part two - surprisingly simple

--- 2023/day13.pas
+++ 2023/day13.pas
@@ -11,7 +11,8 @@ type
   { TDayThirteen}
   TDayThirteen = class(TAocPuzzle)
   private
-  function getMirrorValue(mirror_:TStringArray;mismatches:integer=0):integer;
+  function getMirrorValues(maxErrors:integer=0):integer;
+  function getMirrorValue(mirror_:TStringArray;mismatches:integer):integer;
   function getMismatches(string1,string2:string):integer;
   function getLine(mirror_:TStringArray;lineId: integer):string;
   public
@@ -31,18 +32,26 @@ inherited create(filename,'Day 13',paintbox_);
 end;
 
 procedure TDayThirteen.runPartOne;
+begin
+  results.Clear;
+  results.add('Total is '+getMirrorValues(0).ToString);
+end;
+
+//Almost identical to part one but now we want positions where there is exactly one mismatch.
+procedure TDayThirteen.runPartTwo;
+begin
+  results.Clear;
+  results.add('Total is '+getMirrorValues(1).ToString);
+end;
+
+function TDayThirteen.getMirrorValues(maxErrors: integer): integer;
 var
   currentMirror:TStringArray;
   lineNo,total:integer;
   currentLine:string;
-  mirrorcount,mirrorValue:integer;
-  //topLine,bottomLine:string;
-  //lineDifferences,totalLineDifferences;
 begin
-  results.Clear;
   currentMirror:=TStringArray.create;
-  total:=0;
-  mirrorCount:=1;
+  result:=0;
   currentMirror.clear;
   for lineNo:=0 to pred(puzzleInputLines.size) do
     begin
@@ -51,19 +60,10 @@ begin
       currentMirror.push(currentLine);
     if (currentLine.Length = 0)or(lineNo = pred(puzzleInputLines.size)) then
       begin
-      //Process this mirror
-      mirrorValue:=getMirrorValue(currentMirror);
-      total:=total+mirrorValue;
+      result:=result+getMirrorValue(currentMirror,maxErrors);
       currentMirror.clear;
-      mirrorCount:=mirrorCount+1;
       end;
     end;
-  results.Add('Total is '+total.ToString);
-end;
-
-procedure TDayThirteen.runPartTwo;
-begin
-  results.Clear;
 end;
 
 function TDayThirteen.getMirrorValue(mirror_: TStringArray; mismatches: integer
@@ -79,7 +79,7 @@ var
   begin
   result:= (compareStart < 0) or (compareEnd > limit)
   end;
-
+//Probably could be simplified - quite a lot of repetition in this method!
 begin
   limit:=pred(mirror_.size);
   for hReflection:=1 to limit do
@@ -96,33 +96,33 @@ begin
     compareEnd:=compareEnd+1;
     done:= outOfRange or (mismatchCount > mismatches);
     until done;
-    if (mismatchCount <= mismatches) then
+    if (mismatchCount = mismatches) then
       begin
       result:=100 * (hReflection);
       exit;
       end;
     end;
   limit:=pred(mirror_[0].Length);
-for vReflection:=1 to limit do
-  begin
-  compareStart:=vReflection -1;
-  compareEnd:=vReflection;
-  done:=false;
-  mismatchCount:=0;
-  repeat
-  compLine1:=getLine(mirror_,compareStart);
-  compLine2:=getLine(mirror_,compareEnd);
-  mismatchCount:=mismatchCount + getMismatches(compLine1,compline2);
-  compareStart:=compareStart-1;
-  compareEnd:=compareEnd+1;
-  done:= outOfRange or (mismatchCount > mismatches);
-  until done;
-  if (mismatchCount <= mismatches) then
+  for vReflection:=1 to limit do
     begin
-    result:=vReflection;
-    exit;
+    compareStart:=vReflection -1;
+    compareEnd:=vReflection;
+    done:=false;
+    mismatchCount:=0;
+    repeat
+    compLine1:=getLine(mirror_,compareStart);
+    compLine2:=getLine(mirror_,compareEnd);
+    mismatchCount:=mismatchCount + getMismatches(compLine1,compline2);
+    compareStart:=compareStart-1;
+    compareEnd:=compareEnd+1;
+    done:= outOfRange or (mismatchCount > mismatches);
+    until done;
+    if (mismatchCount = mismatches) then
+      begin
+      result:=vReflection;
+      exit;
+      end;
     end;
-  end;
 results.add('*** No reflection found - your code is probably wrong!');
 result:=-1; //shouldn't get here
 end;
