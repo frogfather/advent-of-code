3dd6526b992890137205997ac358a1fed30cf2ae
frogfather <buyjohn@gmail.com>
2024-02-10 10:50:06 +0000

    outline methods

--- 2023/day19.pas
+++ 2023/day19.pas
@@ -1,12 +1,66 @@
 unit day19;
 
 {$mode objfpc}{$H+}
-
+{$MODESWITCH ADVANCEDRECORDS}
+{$modeswitch TypeHelpers}
 interface
 
 uses
   Classes, SysUtils,  aocPuzzle,LazLogger,ExtCtrls,Graphics,arrayUtils;
 type
+
+  TPart = record
+  fx:integer;
+  fm:integer;
+  fa:integer;
+  fs:integer;
+  fWorkflowsSeen: TStringArray;
+  end;
+
+  TParts = array of TPart;
+
+  { TPartsHelper }
+
+  TPartsHelper = type helper for TParts
+  function size: integer;
+  function push(element:TPart):integer;
+  end;
+
+  TWorkflowRule = record
+  element:string;
+  op:string;
+  value:integer;
+  acceptIfTrue:boolean;
+  rejectIfTrue:boolean;
+  nextWorkflow:boolean;
+  end;
+
+  TWorkflowRules = array of TWorkflowRule;
+
+  { TWorkflowRulesHelper }
+
+  TWorkflowRulesHelper = type helper for TWorkflowRules
+  function size: integer;
+  function push(element:TWorkflowRule):integer;
+  end;
+
+  TWorkflow = class(TInterfacedObject)
+  private
+  fName:string;
+  fRules: TWorkflowRules;
+  public
+  function processPart(part_:TPart):string;
+  end;
+
+  TWorkflows = array of TWorkflow;
+
+  { TWorkflowHelper }
+
+  TWorkflowHelper = type helper for TWorkflows
+  function size: integer;
+  function push(element:TWorkflow):integer;
+  end;
+
 
   { TDayNineteen}
   TDayNineteen = class(TAocPuzzle)
@@ -18,6 +72,53 @@ type
   end;
 
 implementation
+
+{ TPartsHelper }
+
+function TPartsHelper.size: integer;
+begin
+  result:=length(self);
+end;
+
+function TPartsHelper.push(element: TPart): integer;
+begin
+  insert(element,self,length(self));
+  result:=self.size;
+end;
+
+{ TWorkflow }
+
+function TWorkflow.processPart(part_: TPart): string;
+begin
+  //This will run the part through all the rules in this workflow and will return either A,R or the id of another workflow
+  result:='';
+end;
+
+{ TWorkflowHelper }
+
+function TWorkflowHelper.size: integer;
+begin
+  result:=length(self);
+end;
+
+function TWorkflowHelper.push(element: TWorkflow): integer;
+begin
+  insert(element,self,length(self));
+  result:=self.size;
+end;
+
+{ TWorkflowRulesHelper }
+
+function TWorkflowRulesHelper.size: integer;
+begin
+  result:=length(self);
+end;
+
+function TWorkflowRulesHelper.push(element: TWorkflowRule): integer;
+begin
+  insert(element,self,length(self));
+  result:=self.size;
+end;
 
 { TDayNineteen }
 
