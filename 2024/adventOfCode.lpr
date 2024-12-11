4f092895df809a217b6b1f02aa26d12fda37244a
John Campbell <johncampbell@Johns-MacBook-Pro.local>
2024-12-11 07:59:53 +0000

    day 11 parts 1 and 2

--- 2024/adventOfCode.lpr
+++ 2024/adventOfCode.lpr
@@ -11,7 +11,8 @@ uses
   {$ENDIF}
   Interfaces, // this includes the LCL widgetset
   Forms, aoc24, day1, visualise, day2, day3, day4, day5, day6, day7, day8, day9,
-  day10, anysort, aocUtils, arrayUtils, fileUtilities, pointVisitCount
+  day10, anysort, aocUtils, arrayUtils, fileUtilities, pointVisitCount, day11,
+  int64Cache
   { you can add units after this };
 
 {$R *.res}
---
+++ 2024/int64cache.pas
@@ -0,0 +1,70 @@
+unit int64Cache;
+
+{$mode ObjFPC}{$H+}
+{$MODESWITCH ADVANCEDRECORDS}
+{$modeswitch TypeHelpers}
+interface
+
+uses
+  Classes, SysUtils,arrayUtils;
+Type
+
+  TInt64CacheRecord = record
+    input1:int64;
+    input2:int64;
+    output:int64;
+  end;
+
+  TInt64Cache = array of TInt64CacheRecord;
+
+  { TInt64CacheHelper }
+  TInt64CacheHelper = type helper for TInt64Cache
+  function size:integer;
+  function push(rec:TInt64CacheRecord):int64;
+  function indexOf(in1,in2:int64):int64;
+  function outputOf(in1,in2:int64):int64;
+  procedure clear;
+  end;
+
+implementation
+
+{ TInt64CacheHelper }
+
+function TInt64CacheHelper.size: integer;
+begin
+  result:=length(self);
+end;
+
+function TInt64CacheHelper.push(rec: TInt64CacheRecord): int64;
+begin
+  if (self.indexOf(rec.input1,rec.input2) = -1) then
+    begin
+    setLength(self,self.size+1);
+    self[self.size - 1]:=rec;
+    end;
+  result:=self.size;
+end;
+
+function TInt64CacheHelper.indexOf(in1,in2:int64): int64;
+begin
+  result:=-1;
+  if (self.size = 0) then exit;
+  for result:=0 to pred(self.size) do
+    if (self[result].input1 = in1)and(self[result].input2 = in2) then exit;
+  result:=-1;
+end;
+
+function TInt64CacheHelper.outputOf(in1,in2:int64): int64;
+begin
+  if (self.indexOf(in1,in2) = -1) then result:=0
+  else result:=self[indexOf(in1,in2)].output;
+end;
+
+procedure TInt64CacheHelper.clear;
+begin
+  setLength(self,0);
+end;
+
+
+end.
+
