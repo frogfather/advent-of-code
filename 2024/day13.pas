c2217dccebbb58753598738d5e2df49ba8614fbe
John Campbell <johncampbell@Johns-MacBook-Pro.local>
2024-12-14 08:55:46 +0000

    day 14 part 1

--- 2024/day13.pas
+++ 2024/day13.pas
@@ -8,9 +8,19 @@ uses
   Classes, SysUtils,  aocPuzzle,LazLogger,ExtCtrls,Graphics,arrayUtils;
 type
 
+  TDoublePoint = record
+  X:double;
+  Y:double;
+  end;
+
   { TDayThirteen}
   TDayThirteen = class(TAocPuzzle)
   private
+  function minCostOfPrize(machine:TStringArray;useOffset:boolean=false):double;
+  function getButtonMoves(input:String):TDoublePoint;
+  function getPrizeCoords(input:String):TDoublePoint;
+  function getPresses(buttonA,buttonB,prize:TDoublePoint;offset:boolean=false):TDoublePoint;
+  function getTokenCount(usingOffset:boolean=false):double;
   public
   constructor create(filename:string; paintbox_:TPaintbox = nil);
   procedure runPartOne; override;
@@ -21,6 +31,80 @@ implementation
 
 { TDayThirteen }
 
+function TDayThirteen.minCostOfPrize(machine: TStringArray;useOffset:boolean=false): double;
+var
+  buttonA,buttonB:TDoublePoint;
+  prize,presses:TDoublePoint;
+begin
+  buttonA:=getButtonMoves(machine[0]);
+  buttonB:=getButtonMoves(machine[1]);
+  prize:=getPrizeCoords(machine[2]);
+  presses:=getPresses(buttonA,buttonB,prize,useOffset);
+  result:=(3*presses.X) + presses.Y;
+end;
+
+function TDayThirteen.getButtonMoves(input: String): TDoublePoint;
+var
+  x,y:double;
+begin
+  x:=input.Split(':')[1].Trim.Split(',')[0].Trim.Split('+')[1].Trim.ToDouble;
+  y:=input.Split(':')[1].Trim.Split(',')[1].Trim.Split('+')[1].Trim.ToDouble;
+  result.X:=x;
+  result.Y:=y;
+end;
+
+function TDayThirteen.getPrizeCoords(input: String): TDoublePoint;
+var
+  x,y:double;
+begin
+  x:=input.Split(':')[1].Trim.Split(',')[0].Trim.Split('=')[1].Trim.ToDouble;
+  y:=input.Split(':')[1].Trim.Split(',')[1].Trim.Split('=')[1].Trim.ToDouble;
+  result.X:=x;
+  result.Y:=y;
+end;
+
+function TDayThirteen.getPresses(buttonA, buttonB, prize: TDoublePoint;offset:boolean): TDoublePoint;
+var
+  a,b:double;
+  modifiedPrizeX,modifiedPrizeY:double;
+  underHundred:boolean;
+begin
+  result.X:=0;
+  result.Y:=0;
+  if offset then modifiedPrizeX := prize.X + 10000000000000 else modifiedPrizeX:=prize.X;
+  if offset then modifiedPrizeY := prize.Y + 10000000000000 else modifiedPrizeY:=prize.Y;
+  a:= ((modifiedPrizeX*buttonB.Y)-(buttonB.X*modifiedPrizeY))/((buttonB.Y*buttonA.X)-(buttonA.Y*buttonB.X));
+  b:= (modifiedPrizeX-a*buttonA.X)/buttonB.X;
+  underHundred:= (a <= 100) and (b <= 100);
+  if ((frac(a) = 0)and(frac(b) = 0))and (underHundred or offset) then
+    begin
+    result.X:=int(a);
+    result.Y:=int(b);
+    end;
+end;
+
+function TDayThirteen.getTokenCount(usingOffset: boolean): double;
+var
+  index:integer;
+  machine:TStringArray;
+  currentLine:string;
+begin
+  result:=0;
+  machine:=TStringArray.create;
+  for index:=0 to pred(puzzleInputLines.size) do
+    begin
+    currentLine:=puzzleInputLines[index].Trim;
+    if (currentLine <> '')
+      then machine.push(puzzleInputLines[index]);
+    if (currentLine = '') or (index = pred(puzzleInputLines.size))
+      then
+      begin
+        result:=result + minCostOfPrize(machine,usingOffset);
+        machine.clear;
+      end;
+    end;
+end;
+
 constructor TDayThirteen.create(filename:string;paintbox_:TPaintbox);
 begin
 inherited create(filename,'Day 13',paintbox_);
@@ -28,13 +112,16 @@ inherited create(filename,'Day 13',paintbox_);
 end;
 
 procedure TDayThirteen.runPartOne;
+
 begin
   results.Clear;
+  results.add('Total is '+getTokenCount.ToString)
 end;
 
 procedure TDayThirteen.runPartTwo;
 begin
   results.Clear;
+  results.add('Total is '+getTokenCount(true).ToString)
 end;
 
 
--- 2024/day14.pas
+++ 2024/day14.pas
@@ -1,6 +1,8 @@
 unit day14;
 
 {$mode objfpc}{$H+}
+{$MODESWITCH ADVANCEDRECORDS}
+{$modeswitch TypeHelpers}
 
 interface
 
@@ -8,9 +10,36 @@ uses
   Classes, SysUtils,  aocPuzzle,LazLogger,ExtCtrls,Graphics,arrayUtils;
 type
 
+  { TRobot }
+
+  TRobot = class
+  private
+  fpos:TPoint;
+  fvelocity:TPoint;
+  flimits:TRect;
+  public
+  constructor create(position,velocity:TPoint;limits:TRect);
+  procedure move;
+  property position:TPoint read fPos;
+  end;
+
+  TRobots = array of TRobot;
+
+  { TRobotsHelper }
+  TRobotsHelper = type helper for TRobots
+  function size: integer;
+  function push(element:TRobot):integer;
+  procedure clear;
+  end;
+
   { TDayFourteen}
   TDayFourteen = class(TAocPuzzle)
   private
+  procedure loadRobots(grid:TRect);
+  procedure runRobots(times:integer);
+  procedure printRobots;
+  function getQuadrant(quadrantNo:integer;grid:TRect):TRect;
+  function findRobotsInQuadrant(quadrantNo:integer;grid:TRect):integer;
   public
   constructor create(filename:string; paintbox_:TPaintbox = nil);
   procedure runPartOne; override;
@@ -18,18 +47,179 @@ type
   end;
 
 implementation
+  var
+    robots:TRobots;
+    grid:TRect;
+    testGrid:TRect;
+{ TRobot }
+
+constructor TRobot.create(position,velocity:TPoint;limits: TRect);
+begin
+  fPos:=position;
+  fVelocity:=velocity;
+  fLimits:=limits;
+end;
+
+procedure TRobot.move;
+var
+  newX,newY:integer;
+begin
+  newX:=fPos.X + fVelocity.X;
+  newY:=fPos.Y + fVelocity.Y;
+  if (newX < fLimits.Left) then newX:=fLimits.Right + newX + 1;
+  if (newX > fLimits.Right) then newX:= newX - (fLimits.Right + 1);
+  if (newY < fLimits.Top) then newY:=fLimits.Bottom + newY +1;
+  if (newY > fLimits.Bottom) then newY:=newY - (fLimits.Bottom + 1);
+  fPos.X:=newX;
+  fPos.Y:=newY;
+end;
+
+{ TRobotsHelper }
+
+function TRobotsHelper.size: integer;
+begin
+  result:=length(self);
+end;
+
+function TRobotsHelper.push(element: TRobot): integer;
+begin
+  insert(element,self,length(self));
+  result:=self.size;
+end;
+
+procedure TRobotsHelper.clear;
+begin
+  setLength(self,0);
+end;
 
 { TDayFourteen }
+
+procedure TDayFourteen.loadRobots(grid:TRect);
+var
+  index:integer;
+  pos,vel:string;
+  posX,posY,velX,velY:integer;
+begin
+  robots.clear;
+  for index:=0 to pred(puzzleInputLines.size) do
+    begin
+    if puzzleInputLines[index]='' then exit;
+    //each line has position and velocity separated by space
+    pos:= puzzleInputLines[index].Trim.Split(' ')[0].Trim.Split('=')[1].Trim;
+    vel:= puzzleInputLines[index].Trim.Split(' ')[1].Trim.Split('=')[1].Trim;
+    posX:=pos.Split(',')[0].Trim.ToInteger;
+    posY:=pos.Split(',')[1].Trim.ToInteger;
+    velX:=vel.Split(',')[0].Trim.ToInteger;
+    vely:=vel.Split(',')[1].Trim.ToInteger;
+    robots.push(TRobot.create(TPoint.Create(posX,posY),TPoint.Create(velX,velY),grid));
+    end;
+end;
+
+procedure TDayFourteen.runRobots(times: integer);
+var
+  runCount,robotNo:integer;
+begin
+  for runCount:=0 to pred(times) do
+    for robotNo:=0 to pred(robots.size) do
+      robots[robotNo].move;
+end;
+
+procedure TDayFourteen.printRobots;
+var
+  robotNo:integer;
+begin
+  for robotNo:=0 to pred(robots.size)do
+    begin
+    results.Add('Robot '+robotNo.toString+' is at '+robots[robotNo].position.X.toString+':'+robots[robotNo].position.Y.toString);
+    end;
+end;
+
+function TDayFourteen.getQuadrant(quadrantNo: integer; grid: TRect): TRect;
+var
+  gridMiddleH,gridMiddleV:integer;
+begin
+  result:=TRect.Create(0,0,0,0);
+  //quadrant 1 is top left, 2 is bottom left, 3 is top right, 4 is bottom right
+  gridMiddleH:=(grid.Right - grid.Left) div 2;
+  gridMiddleV:=(grid.Bottom- grid.Top) div 2;
+  case quadrantNo of
+  1:
+    begin
+    result.Left:=grid.Left;
+    result.Top:=grid.Top;
+    result.Right:=gridMiddleH - 1;
+    result.Bottom:=gridMiddleV - 1;
+    end;
+  2:
+    begin
+    result.Left:=grid.Left;
+    result.Top:=gridMiddleV + 1;
+    result.Right:=gridMiddleH - 1;
+    result.Bottom:=grid.Bottom;
+    end;
+  3:
+    begin
+    result.Left:=gridMiddleH + 1;
+    result.Top:=grid.Top;
+    result.Right:=grid.Right;
+    result.Bottom:=gridMiddleV - 1;
+    end;
+  4:
+    begin
+    result.Left:=gridMiddleH + 1;
+    result.Top:=gridMiddleV + 1;
+    result.Right:=grid.Right;
+    result.Bottom:=grid.Bottom;
+    end;
+  end;
+end;
+
+function TDayFourteen.findRobotsInQuadrant(quadrantNo: integer; grid: TRect
+  ): integer;
+var
+  quadrant:TRect;
+  index:integer;
+  robotPosition:TPoint;
+begin
+  result:=0;
+  quadrant:=getQuadrant(quadrantNo,grid);
+  for index:=0 to pred(robots.size) do
+    begin
+    robotPosition:=robots[index].position;
+    if (robotPosition.X >= quadrant.Left)
+    and(robotPosition.X <= quadrant.Right)
+    and(robotPosition.Y >= quadrant.Top)
+    and(robotPosition.Y <= quadrant.Bottom)
+    then result:=result+1;
+    end;
+end;
 
 constructor TDayFourteen.create(filename:string;paintbox_:TPaintbox);
 begin
 inherited create(filename,'Day 14',paintbox_);
+robots:=TRobots.create;
+grid:=TRect.Create(0,0,100,102);
+testGrid:=TRect.Create(0,0,10,6);
 //parent loads the file as a string and converts to string array;
 end;
 
 procedure TDayFourteen.runPartOne;
+var
+  quadNo:integer;
+  factor:integer;
+  robotCount:integer;
 begin
   results.Clear;
+  factor:=1;
+  loadRobots(grid);
+  runRobots(100);
+  for quadNo:= 1 to 4 do
+    begin
+    robotCount:=findRobotsInQuadrant(quadNo,grid);
+    results.add('quadrant '+quadNo.toString+': '+robotCount.toString);
+    factor:=factor * robotCount;
+    end;
+  results.add('Safety factor is '+factor.toString);
 end;
 
 procedure TDayFourteen.runPartTwo;
