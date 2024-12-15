e899c3b37266bbd67679f9eedbbb9d2ca7c7585e
John Campbell <johncampbell@Johns-MacBook-Pro.local>
2024-12-15 18:52:52 +0000

    day 15 part 1

--- 2024/day15.pas
+++ 2024/day15.pas
@@ -11,14 +11,19 @@ type
   { TDayFifteen}
   TDayFifteen = class(TAocPuzzle)
   private
-  procedure loadPuzzle;
+  procedure loadPuzzle(larger:boolean=false);
   procedure runPuzzle;
+  procedure runLargePuzzle;
+  function convertRow(row:string;larger:boolean=false):string;
   function findRobotPos:TPoint;
   function nearestSpaceToRobotInGivenDirection(robotPos:TPoint;direction:String):TPoint;
   procedure moveRobotAndBoxes(robotPos,spacePos,offset:TPoint);
   function getRobotOffset(direction:String):TPoint;
   function inBounds(position:TPoint):boolean;
+  function inBounds(row,col:integer):boolean;
   function sumBoxCoordinates:int64;
+  function findItemsToMove(robotPos,robotOffset:TPoint):TPointArray;
+  function moveItems(itemsToMove:TPointArray;offset:TPoint):integer;
   procedure drawPuzzle;
   public
   constructor create(filename:string; paintbox_:TPaintbox = nil);
@@ -34,7 +39,139 @@ var
   instructions:string;
   robotPosition:TPoint;
 
-procedure TDayFifteen.loadPuzzle;
+//methods common to both parts
+
+function TDayFifteen.getRobotOffset(direction: String): TPoint;
+var
+  x,y:integer;
+begin
+  x:=0;
+  y:=0;
+  case direction of
+  '<': x:=-1;
+  '>': x:=1;
+  '^': y:=-1;
+  'v': y:=1;
+  end;
+  result:=TPoint.Create(x,y);
+end;
+
+function TDayFifteen.inBounds(position: TPoint): boolean;
+begin
+  result:=(position.X > 0) and (position.X < pred(map[0].Length))
+       and(position.Y > 0) and (position.Y < pred(map.size));
+end;
+
+function TDayFifteen.inBounds(row, col: integer): boolean;
+begin
+  result:=inBounds(TPoint.create(col,row));
+end;
+
+function TDayFifteen.sumBoxCoordinates: int64;
+var
+  row,col:integer;
+  rString:string;
+begin
+  result:=0;
+  for row:=0 to pred(map.size) do
+    begin
+    rString:=map[row];
+    for col:=0 to pred(rString.Length) do
+      begin
+      if (rString.Substring(col,1) = 'O') then result:=result+(row * 100)+col;
+      end;
+    end;
+end;
+
+function TDayFifteen.findItemsToMove(robotPos, robotOffset: TPoint
+  ): TPointArray;
+var
+  done:boolean;
+  colLeft,colRight,col,row,index:integer;
+  currentRowData:string;
+begin
+  //start at the robot.
+  //for each item, if it has a partial box above we need to add the other half
+  result:=TPointArray.create;
+  done:=false;
+  row:=robotPos.Y;
+  col:=robotPos.X;
+  colLeft:=robotPos.X;  //Only used for vertical
+  colRight:=robotPos.X;
+  row:=robotPos.Y;
+  while not done do
+    begin
+    currentRowData:=map[row].Substring(colLeft,(colRight - colLeft)+1);
+    if (robotOffset.X = 0) then
+      begin
+      if (currentRowData.Substring(0,1) = ']')then colLeft:=colLeft - 1;
+      if (currentRowData.Substring(pred(currentRowData.Length),1) = '[') then colRight:=colRight+1;
+      end;
+    //If the current row is clear we can move
+    if (currentRowData.Replace('.','').Length = 0) then exit;
+    if (currentRowData.IndexOf('#')> -1) then
+      begin
+      result:=TPointArray.create;
+      exit;
+      end else
+      begin
+      //Add the current item to the list and move in the required direction
+      for index:=colLeft to colRight do
+        result.push(TPoint.Create(index,row));
+      row:=row+robotOffset.Y;
+      col:=col+robotOffset.X;
+      colLeft:=colLeft+robotOffset.X;
+      colRight:=colRight+robotOffset.X;
+      if not inBounds(row,col) then
+        begin
+        result:=TPointArray.create;
+        exit;
+        end;
+      end;
+    end;
+end;
+
+function TDayFifteen.moveItems(itemsToMove:TPointArray;offset:TPoint): integer;
+var
+  index:integer;
+  itemToMove:Char;
+begin
+  //move each item on the map
+  for index:= pred(itemsToMove.size) downto 0 do
+    begin
+    itemToMove:=map[itemsToMove[index].Y][itemsToMove[index].X + 1];
+    map[itemsToMove[index].Y + offset.Y][itemsToMove[index].X +offset.X + 1]:=itemToMove;
+    map[itemsToMove[index].Y][itemsToMove[index].X + 1]:='.';
+    if (itemToMove = '@') then
+       begin
+       robotPosition:=TPoint.Create(itemsToMove[index].X +offset.X,itemsToMove[index].Y + offset.Y );
+       end;
+    end;
+  result:=itemsToMove.size;
+end;
+
+procedure TDayFifteen.drawPuzzle;
+var
+  row:integer;
+begin
+  for row:=0 to pred(map.size) do
+    results.add(map[row]);
+end;
+
+function TDayFifteen.findRobotPos: TPoint;
+var
+  row:integer;
+begin
+  for row:= 0 to pred(map.size) do
+    if (map[row].IndexOf('@') > -1) then
+      begin
+      result:=TPoint.Create(map[row].indexOf('@'),row);
+      exit
+      end;
+  result:=TPoint.Create(-1,-1);
+end;
+
+procedure TDayFifteen.loadPuzzle(larger:boolean=false);
 var
   index:integer;
   onInstruction:boolean;
@@ -48,11 +185,12 @@ begin
     else
       begin
       if onInstruction then instructions:=instructions+puzzleInputLines[index]
-      else map.push(puzzleInputLines[index]);
+      else map.push(convertRow(puzzleInputLines[index],larger));
       end;
     end;
 end;
 
+//Part one methods
 procedure TDayFifteen.runPuzzle;
 var
   index:integer;
@@ -67,19 +205,6 @@ begin
     if (nearestSpace.X > -1)and(nearestSpace.Y > -1)
       then moveRobotAndBoxes(robotPosition,nearestSpace,getRobotOffset(instruction));
     end;
-end;
-
-function TDayFifteen.findRobotPos: TPoint;
-var
-  row:integer;
-begin
-  for row:= 0 to pred(map.size) do
-    if (map[row].IndexOf('@') > -1) then
-      begin
-      result:=TPoint.Create(map[row].indexOf('@'),row);
-      exit
-      end;
-  result:=TPoint.Create(-1,-1);
 end;
 
 function TDayFifteen.nearestSpaceToRobotInGivenDirection(robotPos:TPoint;direction: String
@@ -127,49 +252,43 @@ while not done do
   end;
 end;
 
-function TDayFifteen.getRobotOffset(direction: String): TPoint;
-var
-  x,y:integer;
-begin
-  x:=0;
-  y:=0;
-  case direction of
-  '<': x:=-1;
-  '>': x:=1;
-  '^': y:=-1;
-  'v': y:=1;
-  end;
-  result:=TPoint.Create(x,y);
-end;
+//Part two methods
 
-function TDayFifteen.inBounds(position: TPoint): boolean;
-begin
-  result:=(position.X > 0) and (position.X < pred(map[0].Length))
-       and(position.Y > 0) and (position.Y < pred(map.size));
-end;
-
-function TDayFifteen.sumBoxCoordinates: int64;
+procedure TDayFifteen.runLargePuzzle;
 var
-  row,col:integer;
-  rString:string;
+  index:integer;
+  itemsToMove:TPointArray;
+  robotOffset:TPoint;
+  instruction:String;
 begin
-  result:=0;
-  for row:=0 to pred(map.size) do
+  robotPosition:=findRobotPos;
+  //start at the robot and move in the direction indicated
+  for index:=0 to pred(instructions.Length) do
     begin
-    rString:=map[row];
-    for col:=0 to pred(rString.Length) do
-      begin
-      if (rString.Substring(col,1) = 'O') then result:=result+(row * 100)+col;
+    instruction:=instructions.substring(index,1);
+    robotOffset:=getRobotOffset(instruction);
+    itemsToMove:=findItemsToMove(robotPosition,robotOffset);
+    moveItems(itemsToMove,robotOffset);
+    drawPuzzle;
+    end;
+
+end;
+
+function TDayFifteen.convertRow(row: string; larger: boolean): string;
+var
+  element:integer;
+begin
+  result:='';
+  if not larger then result:=row
+  else for element:=0 to pred(row.Length) do
+    begin
+      case row.Substring(element,1) of
+      '#': result:=result + '##';
+      'O': result:=result + '[]';
+      '.': result:=result + '..';
+      '@': result:=result + '@.';
       end;
     end;
-end;
-
-procedure TDayFifteen.drawPuzzle;
-var
-  row:integer;
-begin
-  for row:=0 to pred(map.size) do
-    results.add(map[row]);
 end;
 
 constructor TDayFifteen.create(filename:string;paintbox_:TPaintbox);
@@ -192,6 +311,8 @@ end;
 procedure TDayFifteen.runPartTwo;
 begin
   results.Clear;
+  loadPuzzle(true);
+  runLargePuzzle;
 end;
 
 
