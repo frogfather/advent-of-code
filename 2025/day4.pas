668a17be9009d21534d880daf05553ac93f23b46
John Campbell <j.a.campbell@icloud.com>
2025-12-04 13:43:14 +0000

    part 2

--- 2025/day4.pas
+++ 2025/day4.pas
@@ -13,6 +13,7 @@ type
   private
   procedure loadRolls;
   function getFreeRolls:integer;
+  procedure clearFlaggedRolls;
   public
   constructor create(filename:string; paintbox_:TPaintbox = nil);
   procedure runPartOne; override;
@@ -38,36 +39,48 @@ end;
 function TDayFour.getFreeRolls: integer;
 var
   xindex,yindex,surroundingRolls:integer;
+  rollSymbols:TStringArray;
 begin
   result:=0;
+  rollSymbols:=TStringArray.create;
+  rollSymbols.push('@');
+  rollSymbols.push('X');
   for yindex:=0 to pred(rolls.rows) do
     for xindex:=0 to pred(rolls.size(0))do
       begin
-      //is there a roll there?
-      //is there a roll to the left or right?
-      //if there is a row above, is there a roll above, above-left and above-right?
-      //if there is a row below is there a roll below, below-left and below-right?
       if rolls[yindex][xindex]='@' then
         begin
         surroundingRolls:=0;
-        if (xindex > 0)and(rolls[yindex][xindex-1]='@')then surroundingRolls:=surroundingRolls + 1;
-        if (xindex < pred(rolls.size(yindex)))and(rolls[yindex][xindex+1]='@')then surroundingRolls:=surroundingRolls + 1;
+        if (xindex > 0)and(rollSymbols.indexOf(rolls[yindex][xindex-1])>-1)then surroundingRolls:=surroundingRolls + 1;
+        if (xindex < pred(rolls.size(yindex)))and(rollSymbols.indexOf(rolls[yindex][xindex+1])>-1)then surroundingRolls:=surroundingRolls + 1;
         if (yindex > 0) then
           begin
-          if (xindex > 0)and(rolls[yindex-1][xindex-1]='@')then surroundingRolls:=surroundingRolls + 1;
-          if (rolls[yindex-1][xindex]='@')then surroundingRolls:=surroundingRolls + 1;
-          if (xindex < pred(rolls.size(yindex-1)))and(rolls[yindex-1][xindex+1]='@')then surroundingRolls:=surroundingRolls + 1;
+          if (xindex > 0)and(rollSymbols.indexOf(rolls[yindex-1][xindex-1])>-1)then surroundingRolls:=surroundingRolls + 1;
+          if (rollSymbols.indexOf(rolls[yindex-1][xindex])>-1)then surroundingRolls:=surroundingRolls + 1;
+          if (xindex < pred(rolls.size(yindex-1)))and(rollSymbols.indexOf(rolls[yindex-1][xindex+1])>-1)then surroundingRolls:=surroundingRolls + 1;
           end;
         if (yindex < pred(rolls.rows))then
           begin
-          if (xindex > 0)and(rolls[yindex+1][xindex-1]='@')then surroundingRolls:=surroundingRolls + 1;
-          if (rolls[yindex+1][xindex]='@')then surroundingRolls:=surroundingRolls + 1;
-          if (xindex < pred(rolls.size(yindex+1)))and(rolls[yindex+1][xindex+1]='@')then surroundingRolls:=surroundingRolls + 1;
+          if (xindex > 0)and(rollSymbols.indexOf(rolls[yindex+1][xindex-1])>-1)then surroundingRolls:=surroundingRolls + 1;
+          if (rollSymbols.indexOf(rolls[yindex+1][xindex])>-1)then surroundingRolls:=surroundingRolls + 1;
+          if (xindex < pred(rolls.size(yindex+1)))and(rollSymbols.indexOf(rolls[yindex+1][xindex+1])>-1)then surroundingRolls:=surroundingRolls + 1;
           end;
         if (surroundingRolls < 4)then
+          begin
           result:=result+1;
+          rolls[yindex][xindex]:='X';
+          end;
         end;
       end;
+end;
+
+procedure TDayFour.clearFlaggedRolls;
+var
+  xindex,yindex:integer;
+begin
+  for yindex:=0 to pred(rolls.rows) do
+    for xindex:=0 to pred(rolls.size(0))do
+      if rolls[yindex][xindex]='X' then rolls[yindex][xindex]:='.';
 end;
 
 constructor TDayFour.create(filename:string;paintbox_:TPaintbox);
@@ -89,8 +102,19 @@ begin
 end;
 
 procedure TDayFour.runPartTwo;
+var
+  freeRollCount,totalFreeRollCount:integer;
 begin
+  rolls.clear;
   results.Clear;
+  loadRolls;
+  totalFreeRollCount:=0;
+    repeat
+    freeRollCount:=getFreeRolls;
+    totalFreeRollCount:=totalFreeRollCount + freeRollCount;
+    clearFlaggedRolls;
+    until freeRollCount = 0;
+    results.add('free rolls '+totalfreeRollCount.toString);
 end;
 
 
